24bba7f71ee5d858acca759368fe89936c843caf
farhan ansari <farhanraza2239@gmail.com>
2024-03-02 19:45:31 +0530

    create routing and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
 
 import './App.css'
+import {BrowserRouter as Router,Routes,Route } from "react-router-dom"
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import About from './pages/About'
 
 function App() {
   
 
   return (
-    <>
-      <h1 className=' text-teal-700 text-2xl text-center'>react app</h1>
-    </>
+  <Router>
+    <Routes>
+     < Route path='/' element={<Home/>}/>
+     < Route path='/sign-in' element={<SignIn/>}/>
+     < Route path='/sign-up' element={<SignUp/>}/>
+     < Route path='/profile' element={<Profile/>}/>
+     < Route path='/about' element={<About/>}/>
+    </Routes>
+  </Router>
   )
 }
 
